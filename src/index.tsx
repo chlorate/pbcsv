47237ecb959112d875da877c32da03e9fdf92061
Chlorate <me@chlorate.ca>
2018-06-28 18:10:16 -0300

    Add IndexComponent and render the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,23 @@
-console.log("app");
+import {Component, render} from "inferno";
+import {HashRouter} from "inferno-router";
+import {TabsComponent} from "./nav/tabs.component";
+
+/**
+ * Root component that renders the entire app and holds its state.
+ */
+class IndexComponent extends Component {
+	public render() {
+		return (
+			<HashRouter>
+				<TabsComponent />
+			</HashRouter>
+		);
+	}
+}
+
+const main = document.getElementsByTagName("main");
+if (!main.length) {
+	throw new Error("missing main element");
+}
+
+render(<IndexComponent />, main[0]);
