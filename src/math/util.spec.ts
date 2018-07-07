8ce5aaddb4757ac42e48889f283016ba22373df8
Chlorate <me@chlorate.ca>
2018-07-07 16:30:29 -0300

    Add formatNumber

--- src/math/util.spec.ts
+++ src/math/util.spec.ts
@@ -1,4 +1,24 @@
-import {pad} from "./util";
+import {formatNumber, pad} from "./util";
+
+describe("formatNumber", () => {
+	[
+		{in: 1, out: "1"},
+		{in: 12, out: "12"},
+		{in: 123, out: "123"},
+		{in: 1234, out: "1,234"},
+		{in: 12345, out: "12,345"},
+		{in: 123456, out: "123,456"},
+		{in: 1234567, out: "1,234,567"},
+		{in: 1234567.8, out: "1,234,567.8"},
+		{in: 1234567.89, out: "1,234,567.89"},
+		{in: 1234567.891, out: "1,234,567.891"},
+		{in: 1234567.8912, out: "1,234,567.8912"},
+	].forEach((test) => {
+		it(`should return ${test.out} for ${test.in}`, () => {
+			expect(formatNumber(test.in)).toBe(test.out);
+		});
+	});
+});
 
 describe("pad", () => {
 	it("should pad if number length is less than pad length", () => {
--- src/math/util.ts
+++ src/math/util.ts
@@ -1,4 +1,14 @@
 /**
+ * Returns a number formatted with commas.
+ */
+export function formatNumber(n: number): string {
+	let out = n.toFixed(6).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+	out = out.replace(/0+$/, "");
+	out = out.replace(/\.$/, "");
+	return out;
+}
+
+/**
  * Zero pads a number.
  */
 export function pad(n: number, length: number): string {
