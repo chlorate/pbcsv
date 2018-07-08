785c6258206670dc5d2e73f04248af1b270dc21d
Chlorate <me@chlorate.ca>
2018-07-08 01:44:09 -0300

    Help tab: align to right

--- src/help/help.component.tsx
+++ src/help/help.component.tsx
@@ -5,7 +5,7 @@ import {version, versionDate} from "../version";
  * Contents of the Help tab.
  */
 export const HelpComponent = () => (
-	<Row tag="section" className="justify-content-center">
+	<Row tag="section" className="justify-content-end">
 		<Col md="9" lg="7" xl="6">
 			<Card>
 				<CardHeader tag="h2" className="h3 mb-0">
