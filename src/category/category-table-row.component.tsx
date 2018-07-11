c40167453ad7577e7b13278ff3b1224834df8d42
Chlorate <me@chlorate.ca>
2018-07-11 19:20:43 -0300

    Show values in category table

--- src/category/category-table-row.component.tsx
+++ src/category/category-table-row.component.tsx
@@ -1,6 +1,10 @@
+import {Component} from "inferno";
 import {Badge} from "inferno-bootstrap";
+import {inject} from "inferno-mobx";
 import {Link} from "inferno-router";
 import {ApproxDateComponent} from "../date/approx-date.component";
+import {Model} from "../model/model";
+import {Store} from "../store";
 import {Category} from "./category";
 
 interface Props {
@@ -9,56 +13,24 @@ interface Props {
 	showDate: boolean;
 }
 
+interface InjectedProps extends Props {
+	model: Model;
+}
+
 /**
  * A table row that displays information about a category and its latest run.
  */
-export function CategoryTableRowComponent(props: Props): JSX.Element {
-	const category = props.category;
-	const pb = category.runs[0];
-
-	let versionCell: JSX.Element | undefined;
-	if (props.showVersion) {
-		let platform: JSX.Element | undefined;
-		if (pb.platform) {
-			platform = (
-				<span title={`Platform: ${pb.platform}`}>{pb.platform}</span>
-			);
-		}
-
-		let version: JSX.Element | undefined;
-		if (pb.version) {
-			version = (
-				<Badge title={`Version: ${pb.version}`}>{pb.version}</Badge>
-			);
-		}
-
-		let emulator: JSX.Element | undefined;
-		if (pb.emulator) {
-			emulator = (
-				<Badge color="warning" title={`Emulator: ${pb.emulator}`}>
-					{pb.emulator}
-				</Badge>
-			);
-		}
-
-		versionCell = (
-			<td className="text-nowrap">
-				{platform} {version} {emulator}
-			</td>
-		);
+@inject(Store.Model)
+export class CategoryTableRowComponent extends Component<Props, {}> {
+	get injected(): InjectedProps {
+		return this.props as InjectedProps;
 	}
 
-	let dateCell: JSX.Element | undefined;
-	if (props.showDate) {
-		dateCell = (
-			<td>
-				<ApproxDateComponent date={pb.date} />
-			</td>
-		);
-	}
+	public render(): JSX.Element {
+		const category = this.props.category;
+		const pb = category.runs[0];
 
-	return (
-		<tr>
+		const cells: JSX.Element[] = [
 			<td>
 				<Link
 					className="td-link"
@@ -66,9 +38,57 @@ export function CategoryTableRowComponent(props: Props): JSX.Element {
 				>
 					{category.name}
 				</Link>
-			</td>
-			{versionCell}
-			{dateCell}
-		</tr>
-	);
+			</td>,
+		];
+
+		this.injected.model.valueNames.forEach((name) => {
+			const v = pb.values[name];
+			cells.push(
+				<td className="text-nowrap text-right">{v ? v.string : ""}</td>,
+			);
+		});
+
+		if (this.props.showVersion) {
+			let platform: JSX.Element | undefined;
+			if (pb.platform) {
+				platform = (
+					<span title={`Platform: ${pb.platform}`}>
+						{pb.platform}
+					</span>
+				);
+			}
+
+			let version: JSX.Element | undefined;
+			if (pb.version) {
+				version = (
+					<Badge title={`Version: ${pb.version}`}>{pb.version}</Badge>
+				);
+			}
+
+			let emulator: JSX.Element | undefined;
+			if (pb.emulator) {
+				emulator = (
+					<Badge color="warning" title={`Emulator: ${pb.emulator}`}>
+						{pb.emulator}
+					</Badge>
+				);
+			}
+
+			cells.push(
+				<td className="text-nowrap">
+					{platform} {version} {emulator}
+				</td>,
+			);
+		}
+
+		if (this.props.showDate) {
+			cells.push(
+				<td>
+					<ApproxDateComponent date={pb.date} />
+				</td>,
+			);
+		}
+
+		return <tr>{cells}</tr>;
+	}
 }
--- src/category/category-table.component.tsx
+++ src/category/category-table.component.tsx
@@ -1,4 +1,8 @@
+import {Component} from "inferno";
 import {Card} from "inferno-bootstrap";
+import {inject} from "inferno-mobx";
+import {Model} from "../model/model";
+import {Store} from "../store";
 import {Category} from "./category";
 import {CategoryTableRowComponent} from "./category-table-row.component";
 
@@ -6,76 +10,91 @@ interface Props {
 	categories: Category[];
 }
 
+interface InjectedProps extends Props {
+	model: Model;
+}
+
 /**
  * A table that displays information about categories and their latest runs.
  * Categories not having any runs are not included.
  */
-export function CategoryTableComponent(props: Props): JSX.Element | null {
-	const categories = props.categories.filter((c) => c.runs.length);
-	if (!categories.length) {
-		return null;
+@inject(Store.Model)
+export class CategoryTableComponent extends Component<Props, {}> {
+	get injected(): InjectedProps {
+		return this.props as InjectedProps;
 	}
 
-	let versionHeader: JSX.Element | undefined;
-	const hasPlatforms = categories.some((c) => c.hasPlatforms);
-	const hasVersions = categories.some((c) => c.hasVersions);
-	const hasEmulators = categories.some((c) => c.hasEmulators);
-	if (hasPlatforms || hasVersions || hasEmulators) {
-		// These fields are combined into a single column to keep it compact.
-		// A full header is too long and abbreviating is ugly, so just pick the
-		// most fitting term and show everything as a tooltip.
-		let text = "Emulator";
-		if (hasPlatforms) {
-			text = "Platform";
-		}
-		if (hasVersions) {
-			text = "Version";
+	public render(): JSX.Element | null {
+		const categories = this.props.categories.filter((c) => c.runs.length);
+		if (!categories.length) {
+			return null;
 		}
 
-		const titleParts: string[] = [];
-		if (hasPlatforms) {
-			titleParts.push("Platform");
-		}
-		if (hasVersions) {
-			titleParts.push("Version");
-		}
-		if (hasEmulators) {
-			titleParts.push("Emulator");
+		const header: JSX.Element[] = [<th className="w-100">Category</th>];
+
+		this.injected.model.valueNames.forEach((name) => {
+			header.push(<th className="text-nowrap text-right">{name}</th>);
+		});
+
+		let showVersion = false;
+		const hasPlatforms = categories.some((c) => c.hasPlatforms);
+		const hasVersions = categories.some((c) => c.hasVersions);
+		const hasEmulators = categories.some((c) => c.hasEmulators);
+		if (hasPlatforms || hasVersions || hasEmulators) {
+			// These fields are combined into a single column to keep it
+			// compact. A full header is too long and abbreviating is ugly, so
+			// just pick the most fitting term and show everything as a tooltip.
+			let text = "Emulator";
+			if (hasPlatforms) {
+				text = "Platform";
+			}
+			if (hasVersions) {
+				text = "Version";
+			}
+
+			const titleParts: string[] = [];
+			if (hasPlatforms) {
+				titleParts.push("Platform");
+			}
+			if (hasVersions) {
+				titleParts.push("Version");
+			}
+			if (hasEmulators) {
+				titleParts.push("Emulator");
+			}
+
+			let title: string | undefined;
+			if (titleParts.length > 1) {
+				title = titleParts.join("/");
+			}
+
+			header.push(<th title={title}>{text}</th>);
+			showVersion = true;
 		}
 
-		let title: string | undefined;
-		if (titleParts.length > 1) {
-			title = titleParts.join("/");
+		let showDate = false;
+		if (categories.some((c) => c.hasDates)) {
+			header.push(<th>Date</th>);
+			showDate = true;
 		}
 
-		versionHeader = <th title={title}>{text}</th>;
+		const rows = categories.map((c) => (
+			<CategoryTableRowComponent
+				category={c}
+				showVersion={showVersion}
+				showDate={showDate}
+			/>
+		));
+
+		return (
+			<Card>
+				<table class="table table-bordered table-hover table-responsive card-table">
+					<thead>
+						<tr>{header}</tr>
+					</thead>
+					<tbody>{rows}</tbody>
+				</table>
+			</Card>
+		);
 	}
-
-	let dateHeader: JSX.Element | undefined;
-	if (categories.some((c) => c.hasDates)) {
-		dateHeader = <th>Date</th>;
-	}
-
-	const rows = categories.map((c) => (
-		<CategoryTableRowComponent
-			category={c}
-			showVersion={versionHeader !== undefined}
-			showDate={dateHeader !== undefined}
-		/>
-	));
-
-	return (
-		<Card>
-			<table class="table table-bordered table-hover table-responsive card-table">
-				<thead>
-					<tr>
-						<th className="w-100">Category</th>
-						{versionHeader}
-						{dateHeader}
-					</tr>
-				</thead>
-				<tbody>{rows}</tbody>
-			</table>
-		</Card>
-	);
 }
