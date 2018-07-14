3c3d89b9ea50949eccc9c7c0ab91e5f5aa8e3910
Chlorate <me@chlorate.ca>
2018-07-14 01:56:38 -0300

    Refactor ApproxDateComponent

--- src/date/approx-date.component.tsx
+++ src/date/approx-date.component.tsx
@@ -1,3 +1,4 @@
+import {Component} from "inferno";
 import {ApproxDate, DatePrecision} from ".";
 import {formatNumber} from "../math";
 
@@ -13,41 +14,74 @@ interface Props {
  * Displays a date. Colour indicates how recent it is. Title contains extra
  * information.
  */
-export function ApproxDateComponent(props: Props): JSX.Element | null {
-	const date = props.date;
-	if (!date) {
-		return null;
-	}
-
-	let title = date.fullString;
-
-	let colorClass = "text-muted";
-	if (date.date && date.date.getFullYear() >= new Date().getFullYear()) {
-		colorClass = "text-body";
-	}
-
-	const ago = date.daysAgo;
-	if (ago) {
-		title += "\n";
-		if (date.precision !== DatePrecision.Day) {
-			title += "At least ";
+export class ApproxDateComponent extends Component<Props, {}> {
+	private get colorClass(): string | undefined {
+		const date = this.props.date;
+		if (!date) {
+			return undefined;
 		}
-		title += `${formatNumber(ago)} day${ago === 1 ? "" : "s"} ago.`;
 
-		if (ago <= veryRecentThreshold) {
-			colorClass = "text-success";
-		} else if (ago <= recentThreshold) {
-			colorClass = "text-info";
+		const ago = date.daysAgo;
+		if (ago) {
+			if (ago <= veryRecentThreshold) {
+				return "text-success";
+			} else if (ago <= recentThreshold) {
+				return "text-info";
+			}
 		}
+
+		if (date.date && date.date.getFullYear() >= new Date().getFullYear()) {
+			return "text-body";
+		}
+
+		return "text-muted";
 	}
 
-	return (
-		<time
-			className={`text-nowrap ${colorClass} ${props.className || ""}`}
-			title={title}
-			dateTime={date.iso8601}
-		>
-			{date.string}
-		</time>
-	);
+	private get title(): string | undefined {
+		const date = this.props.date;
+		if (!date) {
+			return undefined;
+		}
+
+		const lines: string[] = [];
+
+		const full = date.fullString;
+		if (full) {
+			lines.push(full);
+		}
+
+		const ago = date.daysAgo;
+		if (ago) {
+			const approx = date.precision !== DatePrecision.Day;
+			lines.push(
+				formatNumber(ago) +
+					(approx ? "+" : "") +
+					` day${ago !== 1 || approx ? "s" : ""} ago`,
+			);
+		}
+
+		return lines.join("\n");
+	}
+
+	public render(): JSX.Element | null {
+		const date = this.props.date;
+		if (!date) {
+			return null;
+		}
+
+		const classNames = ["text-nowrap", this.colorClass];
+		if (this.props.className) {
+			classNames.push(this.props.className);
+		}
+
+		return (
+			<time
+				className={classNames.join(" ")}
+				title={this.title}
+				dateTime={date.iso8601}
+			>
+				{date.string}
+			</time>
+		);
+	}
 }
