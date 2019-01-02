984d0d9f3d52999784e92e84a30df49a688ba6ae
Chlorate <me@chlorate.ca>
2019-01-01 23:34:02 -0400

    Add custom message date formats

--- src/i18n/util.ts
+++ src/i18n/util.ts
@@ -1,5 +1,17 @@
 import IntlMessageFormat from "intl-messageformat";
 
+const customFormats = {
+	date: {
+		month: {
+			month: "long",
+			year: "numeric",
+		},
+		year: {
+			year: "numeric",
+		},
+	},
+};
+
 /**
  * Initialize IntlMessageFormat for each message in an object and return an
  * object with the format function for each message.
@@ -9,7 +21,12 @@ export function createMessages(messages: {
 }): {[key: string]: (values?: object) => string} {
 	const formats = {};
 	Object.keys(messages).forEach((key) => {
-		formats[key] = new IntlMessageFormat(messages[key], "en").format;
+		const message = new IntlMessageFormat(
+			messages[key],
+			"en",
+			customFormats,
+		);
+		formats[key] = message.format;
 	});
 	return formats;
 }
